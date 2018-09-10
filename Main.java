51650371b40f23038962d37e2c5626bfd1551b73
Drogheda <2111037530@qq.com>
2018-09-10 23:45:39 +0800

    twice

--- Main.java
+++ Main.java
@@ -1,72 +1,140 @@
-package test;
+import java.util.Scanner;
 
-class TreeNode{
-    int data, begin, end;
+//import com.sun.corba.se.impl.orbutil.graph.Node;
+
+class TreeNode {
+	long data, lazy;
+	int begin, end;
     TreeNode left, right;
-    TreeNode(int data, int begin, int end) {
+
+    TreeNode(long data, int begin, int end) {
+        this.lazy = 0;
         this.data = data;
         this.begin = begin;
         this.end = end;
         left = right = null;
     }
 }
+
 class SegmentTree {
     int length;
     TreeNode tree;
 
-    public SegmentTree(int length) {
-        this.length = length;
-        init(this.tree = new TreeNode(0, 0, length - 1));
-    }
-    public boolean set(int i, int x){
-        if(i >= 0 && i < length) {
-            update(tree, i, x);
-            return true;
-        }
-        return false;
-    }
-    public int sum(int L, int R){
-        return count(tree, L, R);
-    }
     private void init(TreeNode node) {
-        if(node.begin < node.end) {
+        if (node.begin < node.end) {
             int mid = node.begin + node.end >> 1;
             init(node.left = new TreeNode(0, node.begin, mid));
             init(node.right = new TreeNode(0, mid + 1, node.end));
         }
     }
-    private void update(TreeNode node, int i, int x) {
-        if(node.begin < node.end){
-            int mid = node.begin + node.end >> 1;
-            if(i <= mid) update(node.left, i, x);
-            else update(node.right, i, x);
-            node.data = node.left.data + node.right.data;
+
+    private void updateLazy(TreeNode node) {
+        if (node.begin < node.end) {
+            node.left.lazy += node.lazy;
+            node.right.lazy += node.lazy;
         }
-        else {
-            node.data = x;
-        }
+
+        node.data += (node.end - node.begin + 1) * node.lazy;
+        node.lazy = 0;
     }
-    private int count(TreeNode node, int L, int R) {
-        if(L <= node.begin && R >= node.end){
+
+    private long addBatch(TreeNode node, int L, int R, int x) {
+        if (L <= node.begin && R >= node.end) {
+            node.lazy += x;
+        }
+
+        if (node.lazy != 0) {
+            updateLazy(node);
+        }
+
+        if (L > node.end || R < node.begin) {
             return node.data;
         }
-        int result = 0;
-        int mid = node.begin + node.end >> 1;
-        if(L <= mid) result += count(node.left, L, R);
-        if(R> mid) result += count(node.right, L, R);
+
+        if (L <= node.begin && R >= node.end) {
+            return node.data;
+        }
+
+        long result = 0;
+        result += addBatch(node.left, L, R, x);
+        result += addBatch(node.right, L, R, x);
+
+        return node.data = result;
+    }
+
+    private long countWithLazy(TreeNode node, int L, int R) {
+        if (node.lazy != 0) {
+            updateLazy(node);
+        }
+
+        if (L > node.end || R < node.begin) {
+            return 0;
+        }
+
+        if (L <= node.begin && R >= node.end) {
+            return node.data;
+        }
+
+        long result = 0;
+        result += countWithLazy(node.left, L, R);
+        result += countWithLazy(node.right, L, R);
 
         return result;
     }
 
-};
+    public SegmentTree(int length) {
+        this.length = length;
+        init(this.tree = new TreeNode(0, 0, length - 1));
+    }
+
+    public boolean add(int L, int R, int x) {
+        if (L >= tree.begin && R <= tree.end) {
+            addBatch(tree, L, R, x);
+            return true;
+        }
+
+        return false;
+    }
+
+    public long sum(int L, int R) {
+        return countWithLazy(tree, L, R);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        SegmentTree st = new SegmentTree(10);
-
-        for(int i = 0; i < 10; i++){
-            st.set(i, i);
+    	Scanner sc = new Scanner(System.in);
+    	int N, Q;
+    	N = sc.nextInt();
+    	Q = sc.nextInt();
+        SegmentTree st = new SegmentTree(N);
+        
+        for (int i = 0; i < N; i++) {
+        	int x;
+        	x = sc.nextInt();
+            st.add(i, i, x);
         }
-        System.out.println(st.sum(0, 4));
+        for(int i = 0; i < Q; i++) {
+        	java.lang.String s = sc.next();
+        	char c = s.charAt(0);
+        	if(c == 'Q') {
+        		int l, r;
+        		l = sc.nextInt();
+        		r = sc.nextInt();
+        		System.out.println(st.sum(l - 1, r - 1));
+        	}
+        	else {
+        		int l, r, x;
+        		l = sc.nextInt();
+        		r = sc.nextInt();
+        		x = sc.nextInt(); 
+       			st.add(l - 1, r - 1, x);
+        	}
+        }
     }
 }
+
+
+
+
+
